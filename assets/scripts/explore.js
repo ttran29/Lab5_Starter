51faa07e8ac38f39f1be5cab1e920c113f9bc8f9
JvalaDeus <akhilsubbarao98@gmail.com>
2025-05-06 13:01:55 -0700

    completed implementing explore p1- Speech Synthesis

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,38 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const textInput = document.getElementById('text-to-speak');
+  const voiceSelect = document.getElementById('voice-select');
+  const speakButton = document.getElementById('button');
+  const synthesis = window.speechSynthesis;
+
+  function populateVoiceList() {
+    const voices = synthesis.getVoices();
+    voiceSelect.innerHTML = '';
+
+    voices.forEach((voice) => {
+      const option = document.createElement('option');
+      option.value = voice.name;
+      option.textContent = `${voice.name} (${voice.lang})`;
+      voiceSelect.appendChild(option);
+    });
+  }
+
+  populateVoiceList();
+  if (synthesis.onvoiceschanged !== undefined) {
+    synthesis.onvoiceschanged = populateVoiceList;
+  }
+  speakButton.addEventListener('click', () => {
+    const userInput = textInput.value;
+    const message = new SpeechSynthesisUtterance(userInput);
+    const selectedOptionValue = voiceSelect.selectedOptions[0].getAttribute('value');
+
+    const selectedOption = synthesis.getVoices().find(
+      (voice) => voice.name === selectedOptionValue
+    );
+    if (selectedOption) {
+      message.voice = selectedOption;
+    }
+    synthesis.speak(message);
+  });
 }
